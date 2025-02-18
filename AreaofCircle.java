64d91bd6aae30dbf6a9bc0ab95c7ec12cd823148
Pawar Nitish Rajendra <pawarnitish74@gmail.com>
2025-02-18 23:12:41 +0530

    Add files via upload

---
+++ AreaofCircle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class AreaofCircle{
+
+    public static void main(String[] args){
+
+    double radius,area;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter the radius of the circle");
+    radius = input.nextDouble();
+    input.close();
+
+    area = Math.PI*radius*radius;
+
+    System.out.println("Area of the circle is"+area);
+   }
+}

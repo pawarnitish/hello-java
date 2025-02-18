9593299a30ccf87bae33561fc4ccdfe6cfb4cacb
Pawar Nitish Rajendra <pawarnitish74@gmail.com>
2025-02-18 16:29:10 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+class Sum{
+      public static void main(String args[]){
+        int a = 100;
+        int b = 200;  
+        int sum = 100+200;
+        System.out.println(sum);
+       
+       }
+}

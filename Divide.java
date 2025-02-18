e29b2986b76160d9823b8ff6696cebe5d89e3fee
Pawar Nitish Rajendra <pawarnitish74@gmail.com>
2025-02-18 16:30:11 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,33 @@
+/*class Divide{
+      public static void main(String args[]){
+        int a = 900;
+        int b = 300;
+        int divide = a/b;
+        System.out.println(divide);
+    */ 
+
+/*import java.util.Scanner;
+
+public class Main{
+   
+       public static void main(String [] args){
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("Input the first number: ");
+        int a= input.nextInt();
+        System.out.println("Input the second number: ");
+        int b= input.nextInt();
+
+        int d = (a/b);
+        System.out.println("The division of a and b is: "+d);
+        }
+}*/
+
+public class Divide{
+  public static void main(String[] args){
+  
+  int result = 50/4;
+  
+  System.out.println(result);
+  }
+}

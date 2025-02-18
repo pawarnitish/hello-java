83aa2c7d7490f53d56c775104c54663955857e68
Pawar Nitish Rajendra <pawarnitish74@gmail.com>
2025-02-18 16:33:01 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,35 @@
+/*
+class Multiply{
+        public static void main(String []args){
+        int a = 25;
+        int b = 5;
+      
+         int multiply = a*b;
+
+         System.out.println(multiply);
+         }
+}
+*/
+
+
+/*public class Multiply{
+        public static void main(String []args){
+       // int a = 25;
+       // int b = 5;
+      
+         int multiply = 25*5;
+
+         System.out.println(multiply);
+         }
+}*/
+
+public class Multiply{
+        public static void main(String []args){
+       // int a = 25;
+       // int b = 5;
+      
+         int result= 25*5;
+
+         System.out.println(result);
+         }
+}

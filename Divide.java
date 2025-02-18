13e51045d2d6a8558c81583ecadf7e26df8d2b89
Pawar Nitish Rajendra <pawarnitish74@gmail.com>
2025-02-18 16:31:28 +0530

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -6,22 +6,7 @@
         System.out.println(divide);
     */ 
 
-/*import java.util.Scanner;
 
-public class Main{
-   
-       public static void main(String [] args){
-        Scanner input =new Scanner(System.in);
-
-        System.out.println("Input the first number: ");
-        int a= input.nextInt();
-        System.out.println("Input the second number: ");
-        int b= input.nextInt();
-
-        int d = (a/b);
-        System.out.println("The division of a and b is: "+d);
-        }
-}*/
 
 public class Divide{
   public static void main(String[] args){
